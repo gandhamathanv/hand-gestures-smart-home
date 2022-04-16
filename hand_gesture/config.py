b16f01e27b62a3cc99e95647f5d9de51cc6d501b
Chi <54677538+originalchi@users.noreply.github.com>
2022-04-16 21:30:11 +0700

    Add width for each actions and edit confidence threshold

--- hand_gesture/config.py
+++ hand_gesture/config.py
@@ -1,6 +1,6 @@
-# class_names = ['okay', 'peace', 'thumbs up', 'thumbs down', 'call me', 'stop', 'rock', 'live long', 'fist', 'smile']
 class_names = ['turn_on-off', 'increase_brightness', 'decrease_brightness', 'coloring_temp', 'coloring_randomly', 'coloring_disco']
 class_names_to_idx = {class_name: i for i, class_name in enumerate(class_names)}
+widths = [205, 325, 335, 235, 300, 240]
 
 data_path = './data/dataset.json'
 
@@ -9,4 +9,4 @@ lr=0.0001
 epochs=1000
 gamma = 0.97
 
-confidence_threshold = 0.9
+confidence_threshold = 0.95
