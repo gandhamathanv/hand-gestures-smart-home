Fix model weights with new normalization

hand_gesture/prediction.py
@@ -15,7 +15,7 @@ hand_detection = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_draw        = mp.solutions.drawing_utils
 
 model = HandGestureModel()
-model.load_state_dict(torch.load('./hand_gesture/weights/model.pt'))
+model.load_state_dict(torch.load('./hand_gesture/weights/model_norm.pt'))
 model.eval()
 
 

hand_gesture/train.py
@@ -17,4 +17,4 @@ scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=ga
 
 trainer = Trainer(criterion=criterion, optimizer=optimizer, scheduler=scheduler)
 trainer.train(model=model, train_dataloader=train_dataloader, val_dataloader=val_dataloader,
-              epochs=epochs, save_weights_path='./hand_gesture/weights/model.pt')
+              epochs=epochs, save_weights_path='./hand_gesture/weights/model_norm.pt')